6ec5dc045eafc04eb1ea003001a359f157330f85
Energy <71418770+energy-coresky@users.noreply.github.com>
2024-10-25 20:09:24 +0300

    tiny fix

--- mvc/t_parse.php
+++ mvc/t_parse.php
@@ -28,30 +28,40 @@ class t_parse extends Model_t
         return $this->{$this->_2}($_POST['fn'], $_POST['m']);
     }
 
-    function php_code($fn, $php) {
+    function php_code($php) {
         $tok = $php->tok;
-        $i = 0;
-        $str = function ($next) use (&$i, &$tok) {
-            for ($out = ''; $i < $next; $i++) {
-                [$t, $s] = is_array($tok[$i]) ? $tok[$i] : [0, $tok[$i]];
-                $out .= in_array($t, [T_COMMENT, T_DOC_COMMENT]) ? tag($s, '', 'y') : html($s);
+        $str = function ($y) use (&$tok, $php) {
+            static $i = 0;
+            for ($out = ''; strlen($out) < strlen($y->str); $i++)
+                $out .= is_array($tok[$i]) ? $tok[$i][1] : $tok[$i];
+            yield html($out);
+            if ($i < $y->lst) {
+                $y->rank = false;
+                for ($out = ''; $i < $y->lst; $i++) {
+                    [$t, $s] = is_array($tok[$i]) ? $tok[$i] : [0, $tok[$i]];
+                    $gray = in_array($t, [T_COMMENT, T_DOC_COMMENT, T_INLINE_HTML]);
+                    $out .= $gray ? tag(html($s), '', 'y') : html($s);
+                }
+                yield $out;
             }
-            return $out;
         };
+        $use = fn($y) => in_array($y->rank, [T_CONST, T_CLASS, T_FUNCTION]);
         $out = '';
+        $pos = -1;
         foreach ($php->rank() as $prev => $y) {
-            if (!$y->new)
-                break;
-            $html = $str($y->new->i);
-            if ($y->rank) {
+            $y->lst = $y->new ? $y->new->i : count($tok);
+            foreach ($str($y) as $html) if ($y->rank) {
                 $title = $php->get_real($y);
-                $out .= tag($html, 'title="' . $title . '"', $y->is_def ? 'r' : 'g');
+                $color = $y->is_def ? 'r' : ($use($y) ? 'z' : (T_LIST == $y->rank ? 'm' : 'g'));
+                $out .= tag($html, 'title="' . $title . '"', $color);
+                'CLASS' != $y->rank or $pos = $php->pos;
             } else {
+                if (T_VARIABLE == $y->tok && $php->pos == $pos)
+                    $html = tag($html, 'title="' . '' . '"', 'r');
                 $out .= $html;
+                $php->pos == PHP::_CLASS or $pos = -1;
             }
         }
-        $out .= $str($php->count);
-        //$out = html(file_get_contents($fn));
         echo pre($out);
     }
 
@@ -59,25 +69,29 @@ class t_parse extends Model_t
         $m && SKY::w('last_php_m', $m);
         $m or $m = SKY::w('last_php_m');
         $php = PHP::file($fn, 'minifier' == $m ? 0 : 4);
-        if ($php->syntax_fail)
-            throw new Error($php->syntax_fail);
+        if ($php->parse_error)
+            throw new Error($php->parse_error);
         if ('minifier' == $m || 'beautifier' == $m)
             return print pre(html($php));
         if ('code' == $m)
-            return $this->php_code($fn, $php);
+            return $this->php_code($php);
         $out = '';
         foreach ($php->rank() as $prev => $y) {
+            $ns = '' === $php->ns ? '' : "$php->ns\\";
             if (T_STRING == $y->tok) {
-                $real = in_array($y->rank, ['METHOD', 'CLASS-CONST']) ? $y->str
-                    : ($y->rank ? $php->get_real($y) : '');
-                $s = $php->pos . ".$php->curly $prev $y->line " . token_name($y->tok) . " $real";
+                if ($y->is_def) {
+                    $real = in_array($y->rank, ['NAMESPACE', 'CLASS-CONST', 'METHOD']) ? $y->str : $ns . $y->str;
+                } else {
+                    $real = in_array($y->rank, [T_VAR, T_FN], true) ? $y->str : ($y->rank ? $php->get_real($y) : '');
+                }
+                $s = "$php->pos.$php->curly $prev $y->line " . token_name($y->tok) . " $real";
                 $s .= " ------------------- " . (is_int($y->rank) ? strtolower(token_name($y->rank)) : $y->rank);
                 if ($y->open)
                     $s .= $php->str($y->open, $php->get_close($y));
                 $out .= "===================== \n$s\n";
             } #else { $out .= $php->curly . " == $y->str\n"; }
         }
-        $out = var_export($php->top, true) .
+        $out = var_export($php->head, true) .
             //var_export($php->_tokens_def, true) . 
             $out;
         echo pre(html($out));
